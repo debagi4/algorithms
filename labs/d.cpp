25e7c487b47c139f97a20274c45571fece3d8c16
German Bagdasaryan <german.bagdasaryan@mail.ru>
2025-12-06 17:32:08 +0300

    feat: d

--- labs/d.cpp
+++ labs/d.cpp
@@ -1,29 +1,188 @@
 #include <iostream>
+#include <random>
 #include <vector>
 
 struct PrefixCounter {
 private:
-  std::vector<int> arr_;
-  std::vector<int> prefix_;
+  class Node {
+    friend struct PrefixCounter;
+
+  private:
+    int64_t value_ = 0;
+    int64_t subtree_sum_ = 0;
+    std::uint64_t priority_ = 0;
+    int subtree_size_ = 1;
+    Node* left_ = nullptr;
+    Node* right_ = nullptr;
+
+  public:
+    explicit Node(const int64_t value, const std::uint64_t priority)
+        : value_(value), subtree_sum_(value), priority_(priority) {
+    }
+
+    static int Size(const Node* node) {
+      return (node != nullptr) ? node->subtree_size_ : 0;
+    }
+
+    static int64_t Sum(const Node* node) {
+      return (node != nullptr) ? node->subtree_sum_ : 0;
+    }
+  };
+
+  std::vector<Node> storage_;
+  Node* oddTreap_ = nullptr;
+  Node* evenTreap_ = nullptr;
+
+  std::uint64_t pr_counter_ = 0;
+
+  static constexpr std::uint64_t KSplitMixGamma = 0x9e3779b97f4a7c15ULL;
+  static constexpr std::uint64_t KMul1 = 0xbf58476d1ce4e5b9ULL;
+  static constexpr std::uint64_t KMul2 = 0x94d049bb133111ebULL;
+  static constexpr unsigned KShift1 = 30U;
+  static constexpr unsigned KShift2 = 27U;
+  static constexpr unsigned KShift3 = 31U;
+
+  static std::uint64_t SplitMix64(std::uint64_t value) {
+    value += KSplitMixGamma;
+    value = (value ^ (value >> KShift1)) * KMul1;
+    value = (value ^ (value >> KShift2)) * KMul2;
+    return value ^ (value >> KShift3);
+  }
+
+  std::uint32_t NextPriority() {
+    return static_cast<std::uint32_t>(SplitMix64(pr_counter_++));
+  }
+
+  Node* CreateNode(int64_t value) {
+    storage_.emplace_back(value, NextPriority());
+    return &storage_.back();
+  }
+
+  static void Pull(Node* node) {
+    node->subtree_size_ = 1 + Node::Size(node->left_) + Node::Size(node->right_);
+    node->subtree_sum_ = node->value_ + Node::Sum(node->left_) + Node::Sum(node->right_);
+  }
+
+  static Node* Merge(Node* node_a, Node* node_b) {
+    if ((node_a == nullptr) || (node_b == nullptr)) {
+      return (node_a != nullptr) ? node_a : node_b;
+    }
+    if (node_a->priority_ > node_b->priority_) {
+      node_a->right_ = Merge(node_a->right_, node_b);
+      Pull(node_a);
+      return node_a;
+    }
+    node_b->left_ = Merge(node_a, node_b->left_);
+    Pull(node_b);
+    return node_b;
+  }
+
+  static void SplitByCount(
+      Node* node, const int elements_count, Node*& left_part, Node*& right_part
+  ) {
+    if (node == nullptr) {
+      left_part = right_part = nullptr;
+      return;
+    }
+    if (Node::Size(node->left_) >= elements_count) {
+      SplitByCount(node->left_, elements_count, left_part, node->left_);
+      right_part = node;
+      Pull(right_part);
+    } else {
+      SplitByCount(
+          node->right_, elements_count - Node::Size(node->left_) - 1, node->right_, right_part
+      );
+      left_part = node;
+      Pull(left_part);
+    }
+  }
+
+  static int64_t RangeSumInPlace(Node*& treap, const int left, const int right) {
+    if (left > right) {
+      return 0;
+    }
+    Node* left_part = nullptr;
+    Node* middle_part = nullptr;
+    Node* right_part = nullptr;
+    SplitByCount(treap, right, middle_part, right_part);
+    SplitByCount(middle_part, left - 1, left_part, middle_part);
+
+    const int64_t ans = Node::Sum(middle_part);
+
+    treap = Merge(Merge(left_part, middle_part), right_part);
+    return ans;
+  }
+
+  static void SwapBlocks(
+      Node*& odd_treap, Node*& even_treap, const int odd_pos, const int even_pos, const int len
+  ) {
+    Node* odd_a = nullptr;
+    Node* odd_b = nullptr;
+    Node* odd_c = nullptr;
+    Node* even_a = nullptr;
+    Node* even_b = nullptr;
+    Node* even_c = nullptr;
+
+    SplitByCount(odd_treap, odd_pos - 1, odd_a, odd_b);
+    SplitByCount(odd_b, len, odd_b, odd_c);
+
+    SplitByCount(even_treap, even_pos - 1, even_a, even_b);
+    SplitByCount(even_b, len, even_b, even_c);
+
+    odd_treap = Merge(Merge(odd_a, even_b), odd_c);
+    even_treap = Merge(Merge(even_a, odd_b), even_c);
+  }
 
 public:
-  explicit PrefixCounter(const std::vector<int>& arr) : arr_(arr) {
-    prefix_.assign(arr.size() + 1, 0);
-    prefix_[0] = 0;
-    prefix_[1] = arr[0];
-    for (size_t i = 1; i < arr.size(); i++) {
-      prefix_[i + 1] = prefix_[i] + arr[i];
+  explicit PrefixCounter(const std::vector<int>& arr) {
+    storage_.reserve(arr.size());
+
+    for (int idx = 1; idx <= static_cast<int>(arr.size()); ++idx) {
+      Node* node = CreateNode(arr[idx - 1]);
+      if ((idx % 2) != 0) {
+        oddTreap_ = Merge(oddTreap_, node);
+      } else {
+        evenTreap_ = Merge(evenTreap_, node);
+      }
     }
   }
 
   void Swap(const int left, const int right) {
-    for (int j = left; j <= right; j += 2) {
-      prefix_[j] += arr_[j] - arr_[j - 1];
-      std::swap(arr_[j], arr_[j - 1]);
+    const int len = (right - left + 1) / 2;
+    if (len <= 0) {
+      return;
+    }
+
+    if ((left % 2) != 0) {
+      const int pos = (left + 1) / 2;
+      SwapBlocks(oddTreap_, evenTreap_, pos, pos, len);
+    } else {
+      const int even_pos = left / 2;
+      const int odd_pos = even_pos + 1;
+      SwapBlocks(oddTreap_, evenTreap_, odd_pos, even_pos, len);
     }
   }
-  int GetSum(const int left, const int right) const {
-    return prefix_[right] - prefix_[left - 1];
+
+  int64_t GetSum(const int left, const int right) {
+    int64_t ans = 0;
+
+    const int first_odd = (left % 2 == 1) ? left : left + 1;
+    const int last_odd = (right % 2 == 1) ? right : right - 1;
+    if (first_odd <= last_odd) {
+      const int new_left = (first_odd + 1) / 2;
+      const int new_right = (last_odd + 1) / 2;
+      ans += RangeSumInPlace(oddTreap_, new_left, new_right);
+    }
+
+    const int first_even = (left % 2 == 1) ? left + 1 : left;
+    const int last_even = (right % 2 == 1) ? right - 1 : right;
+    if (first_even <= last_even) {
+      const int new_left = first_even / 2;
+      const int new_right = last_even / 2;
+      ans += RangeSumInPlace(evenTreap_, new_left, new_right);
+    }
+
+    return ans;
   }
 };
 
